932f8730a2800a7589bc86b1b04a28bcc37f8be1
mihir bachhav <mihirbachhav025@gmail.com>
2023-03-08 00:11:52 +0530

    custom token

--- controllers/login.js
+++ controllers/login.js
@@ -16,8 +16,14 @@ const loginuser  = async (req,res) =>{
     try{
         const [rows] = await req.app.locals.pool.execute('SELECT * FROM appuser WHERE EnrollID = ? AND Password = ?', [username, password]);
         if (rows.length === 1) {
-            const token  = jwt.sign({username},process.env.JWT_SECRET,{expiresIn:'30d'});
-            res.status(200).json({msg:'uer created',user : username,token:token});
+             req.app.locals.admin.auth().createCustomToken(username).then((customToken)=>{
+                res.status(200).json({msg:'uer created',user : username,token:customToken});
+             }).catch(()=>{
+                console.log('Error');
+             })
+             //commented  out our own token to use custom firebase token
+            //const token  = jwt.sign({username},process.env.JWT_SECRET,{expiresIn:'30d'});
+          //  res.status(200).json({msg:'uer created',user : username,token:token});
           } else {
             res.status(401).json({ message: 'Invalid credentials' });
         }

6f4ded509cdc3332aa3886f5c7f98ef5c10e032b
mihir bachhav <mihirbachhav025@gmail.com>
2023-03-08 12:45:20 +0530

    added firease token

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,17 +2,36 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = async (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
+  //const authHeader = req.headers.authorization; //same for firebase and old jwt
+  const idToken = req.headers.authorization.split('Bearer ')[1];
+  if (!idToken) {
     return res.status(401).json({ message: 'Authorization header not found' });
   }
 
-  const token = authHeader.split(' ')[1];
+ 
+  console.log(idToken);
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded.username; // add the user information to the request object
-    next(); // pass control to the next middleware
-  } catch (err) {
-    return res.status(401).json({ message: 'Invalid token' });
+    console.log('calling firebase auth');
+    const decodedToken = await req.app.locals.admin.auth().verifyIdToken(idToken);
+  //  const decodedToken = await req.app.locals.admin.auth().verifyIdToken(idToken);
+    console.log(decodedToken);
+    // The JWT is valid
+    // You can access the user's UID, email, etc. from decodedToken
+    const uid = decodedToken.uid;
+    // const email = decodedToken.email;
+    // Do something with the user's data
+    req.user  = uid;
+    next();
+  } catch (error) {
+    // The JWT is invalid
+    console.log(error);
+    res.status(401).send('Unauthorized');
   }
+  // try {
+  //   const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  //   req.user = decoded.username; // add the user information to the request object
+  //   next(); // pass control to the next middleware
+  // } catch (err) {
+  //   return res.status(401).json({ message: 'Invalid token' });
+  // }
 };
